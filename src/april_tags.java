e02a598c3eff4b830fe262d74c03568ec7a066f8
Maple444 <134165739+Maple444@users.noreply.github.com>
2023-10-12 22:17:15 -0400

    Added comments to april_tag.java for clarity

--- src/april_tags.java
+++ src/april_tags.java
@@ -1,10 +1,15 @@
   public class main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        // Make sure to initialize a webcam to use 
+        /* Check the Google Doc for more information on this code! It has a
+         * lot of information about this code and april tags as a whole! */
+
 
         public void runOpMode() throws InterruptedException {
             
             AprilTagProcessor tagProcessor = new AprilTagProcessor.builder()
+                // Sets up the april tag functionality for the driver hub FTC software
                 .setDrawAxes(true);
                 .setDrawCubeProjection(true);
                 .setDrawTagID(true);
@@ -24,6 +29,7 @@
                 if (tagProcessor.getDectections().size() > 0) {
                     AprilTagDetection tag = tagProcessor.getDetections().get(0);
 
+                    // Telemetry data is stored using the below:
                     telemetry.addData("x", tag.ftcPose.x);
                     telemetry.addData("y", tag.ftcPose.y);
                     telemetry.addData("z", tag.ftcPose.z);
