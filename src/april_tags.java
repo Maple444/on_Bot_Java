93474cd6df7bf6f6fdbab6bb4c70743c3eb908df
Maple444 <134165739+Maple444@users.noreply.github.com>
2023-10-14 16:34:31 -0400

    Updates to april_tags.java. Imports added.

--- src/april_tags.java
+++ src/april_tags.java
@@ -1,42 +1,52 @@
-  public class main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        // Make sure to initialize a webcam to use 
-        /* Check the Google Doc for more information on this code! It has a
-         * lot of information about this code and april tags as a whole! */
+package org.firstinspires.ftc.teamcode;
 
+import android.util.Size;
 
-        public void runOpMode() throws InterruptedException {
-            
-            AprilTagProcessor tagProcessor = new AprilTagProcessor.builder()
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+@TeleOp
+public class Vision extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
                 // Sets up the april tag functionality for the driver hub FTC software
-                .setDrawAxes(true);
-                .setDrawCubeProjection(true);
-                .setDrawTagID(true);
-                .setDrawTagOutline(true);
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
                 .build();
 
-            VisionPortal visionPortal = new VisionPortal.Builder()
-                .addProcessor(tagProcessor);
-                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-                .setCameraResolution(new Size(640, 480));
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
                 .build();
-            
-            waitForStart();
 
-            while (!isStopRequested() && opModeIsActive()) {
+        waitForStart();
 
-                if (tagProcessor.getDectections().size() > 0) {
-                    AprilTagDetection tag = tagProcessor.getDetections().get(0);
+        while (!isStopRequested() && opModeIsActive()) {
 
-                    // Telemetry data is stored using the below:
-                    telemetry.addData("x", tag.ftcPose.x);
-                    telemetry.addData("y", tag.ftcPose.y);
-                    telemetry.addData("z", tag.ftcPose.z);
-                    telemetry.addData("roll", tag.ftcPose.roll);
-                    telemetry.addData("pitch", tag.ftcPose.pitch);
-                    telemetry.addData("yaw", tag.ftcPose.yaw);
-                }
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+
+                // Telemetry data is stored using the below:
+                telemetry.addData("x", tag.ftcPose.x);
+                telemetry.addData("y", tag.ftcPose.y);
+                telemetry.addData("z", tag.ftcPose.z);
+                telemetry.addData("roll", tag.ftcPose.roll);
+                telemetry.addData("pitch", tag.ftcPose.pitch);
+                telemetry.addData("yaw", tag.ftcPose.yaw);
             }
+
+            telemetry.update();
         }
     }
+}
